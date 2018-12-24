8fa6a79026dee7db0043dfc41d06c728efd7a205
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2018-12-24 11:16:55 +0530

    set2 17th.c

---
+++ set2 17th.c
@@ -0,0 +1,27 @@
+void main()
+{
+int n,t,temp,rem=0,sum=0;
+scanf("%d",&n);
+t=n;
+temp=n;
+while(n>0)
+{
+n=n/10;
+c++;
+}
+whlie(t>0)
+{
+rem=t%10;
+sum=sum+pow(rew,c);
+t=t/10;
+}
+if(sum==temp)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
