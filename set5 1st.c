b32f707575580c27f5124dfa4130e128b487ec6b
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-01-20 18:55:13 +0530

    set5 1st.c

---
+++ set5 1st.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include<string.h>
+
+int main()
+{
+    int i=0,len=0;
+    char a[20];
+    scanf("%s",&a);
+    scanf("%d",len);
+    for(i=0;i<len,i++)
+    {
+        printf("%s",a);
+    }
+
+    return 0;
+}
