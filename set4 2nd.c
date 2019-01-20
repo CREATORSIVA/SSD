fba1c9538d7875091e5cf3ff0db2e5fbbe72ebe8
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-01-20 14:01:07 +0530

    set4 2nd.c

---
+++ set4 2nd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+chara[50],i=0,c=0,word=1;
+while(a[i]!='\0')
+{
+i++;
+}
+for(j=0;j<i;j++)
+{
+if(a[i]!=' ' && a[j+1]==' ')
+word=word+1;
+}
+printf("%d",word);
+getch();
+}
