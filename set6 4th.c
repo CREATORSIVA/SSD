22c87a687cf832b3d4d87f4ca70aa53b2b37b77c
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-01-21 21:48:53 +0530

    set6 4th.c

---
+++ set6 4th.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,rem=0,sum=0;
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("%d",n);
+    }
+    else
+    {
+        printf("%d",n-1);
+        
+    }
+    return 0;
+}
