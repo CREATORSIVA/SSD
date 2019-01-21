c83b68c4803b279686e9083a62c788a6117410d5
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-01-21 21:52:57 +0530

    set6 6th.c

---
+++ set6 6th.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,pro;
+    scanf("%d %d",&a,&b);
+    pro=a*b;
+    if(pro%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    
+    return 0;
+}
