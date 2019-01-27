b7f14d3b9080c4c38fafd98ffd7ab4c7e441f261
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-01-27 21:02:39 +0530

    Update set6 10th.c

--- set6 10th.c
+++ set6 10th.c
@@ -4,10 +4,17 @@ int main()
 {
 int n,k=0,i;
 scanf("%d",&n);
+    if(n<100000)
+    {
 for(i=1;i<=n;i++)
 {
     k=k+i;
 }
 printf("%d",k);
+    }
+    else
+    {
+        printf("invalid ip");
+    }
 return 0;
 }
