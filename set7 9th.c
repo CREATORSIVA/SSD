b2f0a033727310638538ebe0a6ab4551aa7c8e54
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-02-03 13:57:59 +0530

    Create set7 9th.c

---
+++ set7 9th.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+ int a,b,add=0;
+ scanf("%d%d",&a,&b);
+ add=a+b;
+ if(add%2==0)
+ {
+     printf("even");
+ }
+ else
+ {
+     printf("odd");
+ }
+     return 0;
+}
