ff94d6afcbea8cc90e58b2f5c9aa524053cc4e3c
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-02-12 21:53:42 +0530

    Create set 11 7th.c

---
+++ set 11 7th.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{ int j;
+    int a,b,c,val=0;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    val=(a*b)/c;
+    printf("%d",val);
+    
+    
+    return 0;
+
+    
+}
