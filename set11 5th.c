50764536116fd7712313323dd83db0570878deeb
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-02-12 21:40:42 +0530

    Create set11 5th.c

---
+++ set11 5th.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{ int j;
+    char n[30],k[30],val[30];
+    scanf("%s",n);
+    scanf("%s",k);
+    
+    printf("%s",strcat(n,k));
+    return 0;
+}
