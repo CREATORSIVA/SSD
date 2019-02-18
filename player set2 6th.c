600da486662b77d78680c1e346bbb0f314aebb4e
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-02-18 23:31:39 +0530

    Create player set2 6th.c

---
+++ player set2 6th.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+
+
+int main(void) 
+
+{
+
+	int n,i,j,num,min,count=0;
+
+	int array[100];
+
+	scanf("%d",&n);
+
+	for(i=0;i<n;i++)
+
+	{
+
+		scanf("%d ",&array[i]);
+
+	}
+
+	for(i=0;i<n;i++)
+
+	{
+
+		 count=0;
+
+		for(j=i+1;j<n;j++)
+
+		{
+
+			if(array[i]==array[j] )
+
+			{
+
+				count=count+1;	
+
+			}
+
+			
+
+		}
+
+	if(count==0)
+
+	{
+
+		printf("%d",array[i]);
+
+	}
+
+	}
+
+
+
+	
+
+	
+
+	return 0;
