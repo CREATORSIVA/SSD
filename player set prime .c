70ce92dd27f946837a84e8c759f0a0d6c2041d71
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-02-21 20:48:48 +0530

    Update player set prime .c

--- player set prime .c
+++ player set prime .c
@@ -1,39 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
- 
-void main()
+
+
+
+int main(void) 
+
 {
-    int num1, num2, i, j, flag, temp, count = 0;
- 
-    printf("Enter the value of num1 and num2 \n");
-    scanf("%d %d", &num1, &num2);
-    if (num2 < 2)
-    {
-        printf("There are no primes upto %d\n", num2);
-        exit(0);
-    }
-    printf("Prime numbers are \n");
-    temp = num1;
-    if ( num1 % 2 == 0)
-    {
-        num1++;
-    }
-    for (i = num1; i <= num2; i = i + 2)
-    {
-        flag = 0;
-        for (j = 2; j <= i / 2; j++)
-        {
-            if ((i % j) == 0)
-            {
-                flag = 1;
-                break;
-            }
-        }
-        if (flag == 0)
-        {
-            printf("%d\n", i);
-            count++;
-        }
-    }
-    printf("Number of primes between %d & %d = %d\n", temp, num2, count);
+
+	int l,r,i,j,count=0,flag=1;
+
+	scanf("%d",&l);
+
+	scanf(" %d",&r);
+
+	if(l==2)
+
+	{
+
+		count++;
+
+	}
+
+	for(i=l;i<=r;i++)
+
+	{
+
+		for(j=2;j<i;j++)
+
+		{
+
+			if(i%j==0)
+
+			{
+
+				flag=0;
+
+				break;
+
+				}
+
+			}
+			if(flag==1)
+			{
+                count=count+1;
+
+	}
+
+			}
+	printf("\n%d",count);
+	return 0;
 }
