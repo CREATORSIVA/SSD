f62cb434cefda142b21bf143af4e67d211a015e1
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-02-27 19:36:45 +0530

    Update player set2 10th.c

--- player set2 10th.c
+++ player set2 10th.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 int main()
@@ -9,10 +10,19 @@ int main()
     scanf("%s",a);
     for(i=0;a[i]!='\0';i++)
     {
-    a[i]=a[i]+3;
-    printf("%c",a[i]);
+    
+    if(a[i]=='z')
+    
+        a[i]='c';
+       else if(a[i]=='y')
+           a[i]='b';
+           else if(a[i]=='x')
+           a[i]='a';
+           else
+           
+               a[i]=a[i]+3;
     }
+    printf("%s",a);
 
     return 0;
 }
-
